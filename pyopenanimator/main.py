baa1f931f076dce33088fd7724fcb723f7686939
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2018-05-15 12:10:33 -0400

    Left and Right mix up resolved

--- pyopenanimator/main.py
+++ pyopenanimator/main.py
@@ -21,14 +21,14 @@ clock = pygame.time.Clock()
 controller = inpp.Controller()
 scaleui.init(window, renderer, controller)
 
-right_panel = scaleui.Panel([0,25,0,20], (0,255,0))
-scaleui.window_panel.add_child(right_panel)
+left_panel = scaleui.Panel([0,25,0,20], (0,255,0))
+scaleui.window_panel.add_child(left_panel)
 
 button_widget = scaleui.Button([20,20,45,45], 'Tis a Button')
-right_panel.add_child(button_widget)
+left_panel.add_child(button_widget)
 
-left_panel = scaleui.Panel([75,0,0,20], (0,0,255))
-scaleui.window_panel.add_child(left_panel)
+right_panel = scaleui.Panel([75,0,0,20], (0,0,255))
+scaleui.window_panel.add_child(right_panel)
 
 bottom_panel = scaleui.Panel([0,0,80,0], (255,0,0))
 scaleui.window_panel.add_child(bottom_panel)
