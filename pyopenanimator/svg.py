ac60f5699791780d5877dab0df559b56ba321924
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2018-05-14 23:31:52 -0400

    Don't load SVGs everytime they are rendered.

--- pyopenanimator/svg.py
+++ pyopenanimator/svg.py
@@ -3,7 +3,12 @@ import cairosvg
 import io
 import constants
 
+
+def load(file):
+	return open(file, "rb")
+
+
 def render(file, scale):
 	if scale <= 0:
 		raise ValueError('''Value "%s" is to low. SVG render scale must be higher than 0.''' % scale)
-	return pygame.image.load(io.BytesIO(cairosvg.svg2png(file_obj=open(file, "rb"), dpi=(constants.SVG_BASE_DPI/100)*scale)), "")
+	return pygame.image.load(io.BytesIO(cairosvg.svg2png(file_obj=file, dpi=(constants.SVG_BASE_DPI/100)*scale)), "")
