6eb6757f6b1324ac097a3d4c264e223735a9ea4f
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2018-05-14 23:20:25 -0400

    Restructure Project

--- pyopenanimator/main.py
+++ pyopenanimator/main.py
@@ -6,7 +6,7 @@ from pygame.render import *
 import random
 
 import inpp
-import gui
+import scaleui
 import svg
 import constants
 
@@ -19,27 +19,27 @@ renderer.render_present()
 
 clock = pygame.time.Clock()
 controller = inpp.Controller()
-gui.init(window, renderer, controller)
+scaleui.init(window, renderer, controller)
 
-right_panel = gui.Panel([0,25,0,20], (0,255,0))
-gui.window_panel.add_child(right_panel)
+right_panel = scaleui.Panel([0,25,0,20], (0,255,0))
+scaleui.window_panel.add_child(right_panel)
 
-button_widget = gui.Button([20,20,45,45], 'Tis a Button')
+button_widget = scaleui.Button([20,20,45,45], 'Tis a Button')
 right_panel.add_child(button_widget)
 
-left_panel = gui.Panel([75,0,0,20], (0,0,255))
-gui.window_panel.add_child(left_panel)
+left_panel = scaleui.Panel([75,0,0,20], (0,0,255))
+scaleui.window_panel.add_child(left_panel)
 
-bottom_panel = gui.Panel([0,0,80,0], (255,0,0))
-gui.window_panel.add_child(bottom_panel)
+bottom_panel = scaleui.Panel([0,0,80,0], (255,0,0))
+scaleui.window_panel.add_child(bottom_panel)
 
-label = gui.Label([10,10,25,25], 'Hello World! I am a Label!')
+label = scaleui.Label([10,10,25,25], 'Hello World! I am a Label!')
 bottom_panel.add_child(label)
 
-gui.update()
+scaleui.update()
 while True:
 	controller.tick()
-	gui.tick()
+	scaleui.tick()
 
 	if controller.events.quit() or controller.events.key(pygame.K_ESCAPE):
 		break
@@ -47,11 +47,11 @@ while True:
 	for current_event in controller.events.raw():
 		if current_event.type == pygame.VIDEORESIZE:
 			window = pygame.display.set_mode((current_event.w, current_event.h), pygame.RESIZABLE)
-			gui.window_panel.parent_size = (current_event.w, current_event.h)
-			gui.window_size = [current_event.w, current_event.h]
-			gui.window_panel.update()
-			gui.resizing_timer = constants.RESIZING_SECONDS*constants.FPS
+			scaleui.window_panel.parent_size = (current_event.w, current_event.h)
+			scaleui.window_size = [current_event.w, current_event.h]
+			scaleui.window_panel.update()
+			scaleui.resizing_timer = constants.RESIZING_SECONDS*constants.FPS
 
-	gui.draw()
+	scaleui.draw()
 	renderer.render_present()
 	clock.tick(constants.FPS)
